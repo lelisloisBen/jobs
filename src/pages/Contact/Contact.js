d4843de1687cfddb2ea909ddf7606588c4ee4969
Samir Benzada <samirbenzada@gmail.com>
2019-09-22 12:24:30 -0400

    UI component Button

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../../components/UI/Button';
 
 function Contact() {
   return (
-    <div>
+    <div className="text-center">
         <h1>Contact page</h1>
+        <Button btnType="Success" disabled={false}>ORDER</Button>
     </div>
   );
 }
