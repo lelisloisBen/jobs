6517146bb3fc3a9251301f26cbaf8067932773d6
samir benzada <samirbenzada@gmail.com>
2019-09-24 23:18:54 -0400

    policies

--- src/pages/Policies/Policies.js
+++ src/pages/Policies/Policies.js
@@ -4,36 +4,107 @@ class Policies extends Component {
     render() {
         return (
             <div>
-                Account Executive
-				Application Developer
-                Account Manager
-                Architect
-                Account Representative
-                Art Director
-                Accountant
-                Assembler
-                Accounting Assistant
-                Assistant
-                Accounting Clerk
-                Assistant Controller
-                Accounting Manager
-                Assistant Editor
-                Accounts Payable Clerk
-                Assistant Manager
-                Accounts Payable Specialist
-                Assistant Project Manager
-                Accounts Receivable Clerk
-                Assistant Property Manager
-                Administrative Assistant
-                Assistant Store Manager
-                Administrative Coordinator
-                Assistant Teacher
-                Administrative Specialist
-                Attorney
-                Analyst
-                Autocad Drafter
-                Analytical Chemist
-                Automotive Technician
+            <h3>Category</h3>
+            Accounting/Finance
+            Healthcare
+            Non-Profit/Volunteering
+            Administrative
+            Computer/Internet
+            Pharmaceutical/Bio-tech
+            Arts/Entertainment/Publishing
+            Hospitality/Travel	
+            Real Estate
+            Banking/Loans	
+            Human Resources	
+            Restaurant/Food Service
+            Construction/Facilities	
+            Insurance	
+            Retail
+            Customer Service	
+            Law Enforcement/Security	
+            Sales
+            Education/Training	
+            Legal	
+            Telecommunications
+            Engineering/Architecture	
+            Manufacturing/Mechanical	
+            Transportation/Logistics
+            Government/Military	
+            Marketing/Advertising/PR	
+            Upper Management/Consulting
+
+
+            <h4>### Category: Accounting/Finance</h4>
+
+    <li>Accountant</li>
+    <li>Director Of Finance</li>
+    <li>Office Manager Bookkeeper</li>
+    <li>Accounting Assistant</li>
+    <li>Finance Manager</li>
+    <li>Quantitative Analyst</li>
+    <li>Accounting Clerk</li>
+    <li>Financial Advisor</li>
+    <li>Quantitative Research Analyst</li>
+    <li>Accounting Manager</li>
+    <li>Financial Analyst</li>
+    <li>Senior Accountant</li>
+    <li>Assistant Controller</li>
+    <li>Financial Associate</li>
+    <li>Tax Accountant</li>
+    <li>Billing Specialist</li>
+    <li>Financial Reporting Manager</li>
+    <li>Tax Manager</li>
+    <li>Bookkeeper</li>
+    <li>Full Charge Bookkeeper</li>
+    <li>Treasury Analyst</li>
+    <li>Budget Analyst</li>
+    <li>General Ledger Accountant</li>
+    <li>VP Director of Finance</li>
+    <li>CNA</li>
+    <li>Internal Auditor</li>
+    <li>VP of Finance</li>
+    <li>Controller</li>
+    <li>Night Auditor</li>
+    <li>WFF USC Credit Manager</li>
+
+
+
+
+
+                <h3>A List </h3>
+                <ul>
+                    <li> Account Executive </li>
+                    <li> Application Developer</li>
+                    <li> Account Manager</li>
+                    <li> Architect</li>
+                    <li> Account Representative</li>
+                    <li> Art Director</li>
+                    <li> Accountant</li>
+                    <li> Assembler</li>
+                    <li> Accounting Assistant</li>
+                    <li> Assistant</li>
+                    <li> Accounting Clerk</li>
+                    <li> Assistant Controller</li>
+                    <li> Accounting Manager</li>
+                    <li> Assistant Editor</li>
+                    <li> Accounts Payable Clerk</li>
+                    <li> Assistant Manager</li>
+                    <li> Accounts Payable Specialist</li>
+                    <li> Assistant Project Manager</li>
+                    <li> Accounts Receivable Clerk</li>
+                    <li> Assistant Property Manager</li>
+                    <li> Administrative Assistant</li>
+                    <li> Assistant Store Manager</li>
+                    <li> Administrative Coordinator</li>
+                    <li> Assistant Teacher</li>
+                    <li> Administrative Specialist</li>
+                    <li> Attorney</li>
+                    <li> Analyst</li>
+                    <li> Autocad Drafter</li>
+                    <li> Analytical Chemist</li>
+                    <li> Automotive Technician</li>
+                </ul>
+            <h3>B List </h3>
             </div>
         )
     }
